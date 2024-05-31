ccfda90ee0dc8228f8a2c417e133dce25248a2ef
Nerdy Gedoni <gedoniani@gmail.com>
2024-05-31 10:49:08 +0100

    cors url update

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const { initSocket } = require('./config/socket');
 dotenv.config();
 
 const corsOptions = {
-    //origin: 'https://www.daeds.uk', 
-    origin: 'http://localhost:5173', 
+    origin: 'https://cartrk.vercel.app/', 
+   // origin: 'http://localhost:5173', 
     credentials: true,
   };
   
